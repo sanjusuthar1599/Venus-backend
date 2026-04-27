ef44c67071f6f80776c1d48ccc59d02e2d7c068b
Sanjay <Sanjusuthar1599@gmail.com>
2026-04-27 18:09:26 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -25,9 +25,10 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ Database connected');
+    return true;
   } catch (err) {
     console.error('❌ Database connection error:', err.message);
-    process.exit(1);
+    return false;
   }
 };
 
